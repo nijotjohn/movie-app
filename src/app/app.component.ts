686b9ff7bbc3d845a507920f04630bcb9f20774d
Rajeev Kanathil Ravi <rajeev.k.ravi@eqs.com>
2019-05-29 14:39:42 +0530

    primeng added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'movie-app';
+export class AppComponent implements OnInit {
+  items: MenuItem[];
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              items: [{
+                      label: 'New', 
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {label: 'Project'},
+                          {label: 'Other'},
+                      ]
+                  },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              ]
+          }
+      ];
+  }
 }
